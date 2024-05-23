70badbc9618aad347d2a21061cf94993f40fe872
SilNa <50061081+silvernine999@users.noreply.github.com>
2024-05-23 22:29:55 +0900

    장바구니가 될 무언가

---
+++ recipes/cart.js
@@ -0,0 +1,8 @@
+function toggleContent() {
+    var content = document.getElementById("content");
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
